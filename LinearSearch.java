a606ffbf75f45fb4d54dcae4794e44534446c6a5
Elam Rufino <107227033+ElamRS@users.noreply.github.com>
2022-12-08 10:04:23 -0300

    Add files via upload

---
+++ LinearSearch.java
@@ -0,0 +1,22 @@
+package challenge;
+
+import java.util.Random;
+
+// O método de busca linear consiste na forma mais simples de se procurar por um elemento em uma lista,
+// Itera-se do início ao fim do vetor e caso o item seja encontrado, o índice é retornado.
+
+// Possuí uma eficiência de O(1) no melhor caso (elemento procurado no primeiro índice)
+// E de O(n) no pior caso (elemento procurado é o último da lista).
+
+public class LinearSearch {
+	public static int linearSearch(int[] vetor, int elemento) {
+		// Itera-se sobre o vetor a procura do elemento
+		for (int i=0; i<vetor.length; i++) {
+			// Caso ele seja encontrado...
+			if (vetor[i]==elemento) {
+				return i; // O índice do elemento procurado é retornado
+			}
+		}
+		return -1; // Caso o elemento procurado não faça parte da lista -1 é retornado
+	}
+}
